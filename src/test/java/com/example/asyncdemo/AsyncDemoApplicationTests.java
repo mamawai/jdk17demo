0139683f7dfdf0d736b8688152d09d390e88d3b0
mamawai <mmy20000513@163.com>
2023-08-03 16:21:57 +0800

    删除无用代码

--- src/test/java/com/example/asyncdemo/AsyncDemoApplicationTests.java
+++ src/test/java/com/example/asyncdemo/AsyncDemoApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.example.asyncdemo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class AsyncDemoApplicationTests {
 
 	@Test
 	void contextLoads() {
