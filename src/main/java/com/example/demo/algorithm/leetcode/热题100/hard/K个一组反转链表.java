97bc622c5d9c50e5cfb272bd3b10706c7c5213de
mamawai <mmy20000513@163.com>
2024-07-19 10:55:20 +0800

    LC from workplace

--- src/main/java/com/example/demo/algorithm/leetcode/热题100/hard/K个一组反转链表.java
+++ src/main/java/com/example/demo/algorithm/leetcode/热题100/hard/K个一组反转链表.java
@@ -2,31 +2,26 @@ package com.example.demo.algorithm.leetcode.热题100.hard;
 
 import com.example.demo.algorithm.leetcode.structure.ListNode;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class K个一组反转链表 {
 
     public static void main(String[] args) {
-        new Solution25B().reverseKGroup(new ListNode(1,new ListNode(2,new ListNode(3,new ListNode(4,new ListNode(5))))),0);
+        ListNode node = new Solution25().reverseKGroup(new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5))))), 2);
+        System.out.println(node);
     }
 }
 
 class Solution25 {
-    // 一点点来先反转全部
-    // 5 4 3 2 1
-    // 1 2 3 4 5
     ListNode dummy;
-    ListNode linkHead;
-    List<ListNode[]> nodeList;
+    ListNode tail;
+    ListNode preTail;
+    ListNode realHead;
     int oriK;
     public ListNode reverseKGroup(ListNode head, int k) {
         oriK = k;
-        nodeList = new ArrayList<>();
         dummy = new ListNode();
         dummy.next = head;
         dfs(head, k - 1);
-        return dummy.next;
+        return realHead;
     }
 
     private void dfs(ListNode node, int k) {
@@ -34,45 +29,24 @@ class Solution25 {
         ListNode next = node.next;
         // 连接next 更新dummy
         if (dummy.next != node) {
-            next.next = dummy.next;
-            dummy.next = next;
-        }
-        // 当node为每个片区第一个节点时才更新linkHead
-        if (k == oriK - 1) linkHead = node;
-
-
-
-
-        // 断开node下一个节点
-        node.next = null;
-        // 获取第三个节点的引用
-        ListNode skip = next.next;
-        // 它的下一个节点指向head即dummy.next
-        next.next = dummy.next;
-        dummy.next = next;
-        k--;
+            node.next = dummy.next;
+            dummy.next = node;
+        } else node.next = null;
+        // 当node为每个片区第一个节点时更新tail
+        if (k == oriK - 1) tail = node;
         if (k == 0) {
-            nodeList.add(new ListNode[]{dummy, linkHead});
-            dfs();
-        } else
-    }
-}
-class Solution25B {
-    // 一点点来先反转全部
-    // 5 4 3 2 1
-    // 1 2 3 4 5
-    ListNode dummy;
-    List<ListNode[]> nodeList;
-    int oriK;
-    public ListNode reverseKGroup(ListNode head, int k) {
-        oriK = k;
-        nodeList = new ArrayList<>();
-        dummy = new ListNode();
-        dummy.next = head;
-        // head/head.next
-        nodeList.add(new ListNode[]{dummy.next, dummy.next.next});
-        dummy = dummy.next;
-        nodeList.add(new ListNode[]{dummy.next, dummy.next.next});
-        return new ListNode();
+            if (realHead == null) realHead = dummy.next;
+            if (preTail != null) preTail.next = dummy.next;
+            preTail = tail;
+            if (next == null) return;
+            // 每个片区结束后更新dummy
+            dummy.next = next;
+            dfs(next, oriK - 1);
+        } else {
+            if (next != null) dfs(next, k - 1);
+            // 说明当前遍历到最后一个ListNode且最后一组的数量不足k个
+            // 需要再逆序过来拼接
+            else dfs(dummy.next , oriK - k - 1);
+        }
     }
 }
