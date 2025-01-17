2c04815d07ad6a0ccc53b1a533341aada3776e0d
mamawai <mmy20000513@163.com>
2025-01-17 09:25:12 +0800

    注释掉注解避免springBoot扫描

--- src/main/java/com/example/AppRunner.java
+++ src/main/java/com/example/AppRunner.java
@@ -10,7 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 @Slf4j
 public class AppRunner implements CommandLineRunner {
 
--- src/main/java/com/example/demo/javaPractice/Controller.java
+++ src/main/java/com/example/demo/javaPractice/Controller.java
@@ -4,11 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+//@RestController
 public class Controller {
     @Autowired
     Divide divide;
-
     @RequestMapping("/hello")
     public String getDiv() {
         int div = divide.div(6, 0);
